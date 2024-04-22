d07960c0efda892ab798380a4f645fdb2e049f9a
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-04-22 15:07:09 +0300

    Добавил первый тестовый класс
    Добавил первый тестовый класс

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class FirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnK")).click();
+        //System.out.println(driver.getTitle());
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @AfterEach
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
